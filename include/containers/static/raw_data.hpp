15407b578a2e234e7fe814ba3e5544647af8087e
Ricky Doofenshmirtz <RickyDoofenshmirtz@gmail.com>
2026-03-23 00:48:38 +0530

    added raw data class

---
+++ include/containers/static/raw_data.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <cstddef>
+#include <exception>
+#include <memory>
+#include <new>
+#include <optional>
+#include <type_traits>
+#include <utility>
+
+template <typename T>
+    requires std::is_same_v<T, std::remove_cvref_t<T>>
+class raw_data
+{
+public:
+    explicit constexpr raw_data() noexcept = default;
+
+    constexpr auto void_ptr() noexcept -> void* { return static_cast<void*>(m_data); }
+    constexpr auto void_ptr() const noexcept -> const void*
+    {
+        return static_cast<const void*>(m_data);
+    }
+
+    // NOLINTBEGIN
+    // clang-format off
+    constexpr auto data_ptr() noexcept -> T* { return std::launder(reinterpret_cast<T*>(m_data)); }
+    constexpr auto data_ptr() const noexcept -> const T*  { return std::launder(reinterpret_cast<const T*>(m_data)); }
+    // NOLINTEND
+    // clang-format on
+
+    constexpr auto value() noexcept -> T& { return *data_ptr(); }
+    constexpr auto value() const noexcept -> const T& { return *data_ptr(); }
+
+    constexpr void clear() noexcept { std::destroy_at(data_ptr()); }
+
+    template <typename... Args>
+        requires(std::is_nothrow_constructible_v<T, Args...> && std::is_nothrow_destructible_v<T>)
+    constexpr auto emplace(Args&&... args) noexcept -> T&
+    {
+        clear();
+        auto ptr = new(void_ptr()) T(std::forward<Args>(args)...);
+        return *ptr;
+    }
+
+    template <typename... Args>
+        requires(std::is_constructible_v<T, Args...> && std::is_nothrow_destructible_v<T>)
+    constexpr auto try_emplace(Args&&... args) noexcept -> std::optional<T&>
+    {
+        clear();
+        try {
+            auto ptr = new(void_ptr()) T(std::forward<Args>(args)...);
+            return *ptr;
+        } catch (...) {
+            return std::nullopt;
+        }
+    }
+
+    template <typename... Args>
+        requires(std::is_constructible_v<T, Args...> && std::is_nothrow_destructible_v<T>)
+    constexpr auto force_emplace(Args&&... args) noexcept -> T&
+    {
+        clear();
+        try {
+            auto ptr = new(void_ptr()) T(std::forward<Args>(args)...);
+            return *ptr;
+        } catch (...) {
+            std::terminate();
+        }
+    }
+
+private:
+    alignas(T) std::byte m_data[sizeof(T)];
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,17 @@
-#include "containers/static/static_vector.hpp"
-#include "logging.hpp"
-#include "memory/unique_handle.hpp"
-#include "test_handles.hpp"
-
+#include "containers/static/raw_data.hpp"
 #include <print>
+#include <utility>
 
 namespace {
-    [[maybe_unused]]
-    void test_array() noexcept
-    {
-        auto _   = entry_logger{};
-        auto arr = static_vector<coordinate, 3>::construct();
-        arr.emplace_back(1, 2);
-        arr.emplace_back(5, 9);
-        arr.emplace_back(7, 3);
-        for (auto& [x, y] : arr) { std::println("{}, {}", x, y); }
-        std::println("---");
-        if (auto last = arr.pop_back()) {
-            auto [x, y] = *last;
-            std::println("{}, {}", x, y);
-        }
-        std::println("---");
-        for (auto& [x, y] : arr) { std::println("{}, {}", x, y); }
-    }
-
-    [[maybe_unused]]
-    void test_static_vector_emplace() noexcept
-    {
-        auto _     = entry_logger{};
-        auto arr   = static_vector<int, 2>::construct();
-        auto& last = arr.force_emplace_back(1);
-        arr.force_emplace_back(1);
-        arr.force_emplace_back(1);
-        std::println("{}", last);
-        last = 5;
-        std::println("{}", arr.back());
-    }
-
     class coordinate
     {
-        friend optional_handle<coordinate>;
-
     public:
         static auto construct(int x, int y) noexcept -> coordinate { return coordinate{ x, y }; }
 
         auto get_x() const noexcept -> int { return m_x; }
         auto get_y() const noexcept -> int { return m_y; }
+
+        auto get() const noexcept -> std::pair<int, int> { return { m_x, m_y }; }
 
     private:
         explicit coordinate(int x, int y) noexcept
@@ -57,20 +23,18 @@ namespace {
         int m_y;
     };
 
-    [[maybe_unused]]
-    void test_placement_new() noexcept
+    void func() noexcept
     {
-        auto _      = entry_logger{};
-        auto handle = optional_handle<coordinate>{};
-        auto c      = coordinate::construct(3, 4);
-        handle.emplace(c);
+        auto data   = raw_data<coordinate>{};
+        auto [x, y] = data.emplace(coordinate::construct(5, 6)).get();
+        std::println("{}, {}", x, y);
+        data.clear();
     }
-
-} // namespace
+}
 
 int main()
 {
-    auto _ = entry_logger{};
-    test_handles();
+    func();
+    std::println("Exiting");
     return 0;
 }
