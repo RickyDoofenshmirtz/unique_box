a5e9a15fa7beffd712e229c3658479d94bf433de
Ricky Doofenshmirtz <RickyDoofenshmirtz@gmail.com>
2026-03-27 19:03:58 +0530

    fixed addition and subtraction overloads in circular_index class

--- include/containers/static/static_queue.hpp
+++ include/containers/static/static_queue.hpp
@@ -12,52 +12,59 @@ template <std::size_t N>
 class circular_index
 {
 public:
-    circular_index() noexcept = default;
+    constexpr circular_index() noexcept = default;
 
-    circular_index(const std::size_t index) noexcept
+    constexpr circular_index(const std::size_t index) noexcept
         : m_index(index)
     {
     }
 
-    operator std::size_t() const noexcept { return m_index; }
+    constexpr operator std::size_t() const noexcept { return m_index; }
 
-    auto operator<=>(const circular_index&) const noexcept = default;
+    constexpr auto operator<=>(const circular_index&) const noexcept = default;
 
-    auto operator=(const std::size_t i) noexcept -> circular_index&
+    constexpr auto operator=(const std::size_t i) noexcept -> circular_index&
     {
-        m_index = i;
+        m_index = i % N;
         return *this;
     }
 
-    auto operator++() noexcept -> circular_index&
+    constexpr auto operator++() noexcept -> circular_index&
     {
         m_index = (m_index + 1) % N;
         return *this;
     }
 
-    auto operator++(int) noexcept -> circular_index
+    constexpr auto operator++(int) noexcept -> circular_index
     {
         auto old = *this;
         ++(*this);
         return old;
     }
 
-    friend constexpr auto operator-(const circular_index& a, const circular_index& b)
+    friend constexpr auto operator-(const circular_index& a, const circular_index& b) noexcept
         -> circular_index
     {
-        return circular_index{ a.m_index - b.m_index };
+        return circular_index{ (a.m_index - b.m_index + N) % N };
     }
 
-    friend constexpr auto operator+(const circular_index& a, const circular_index& b)
+    friend constexpr auto operator+(const circular_index& a, const circular_index& b) noexcept
         -> circular_index
     {
-        return circular_index{ a.m_index + b.m_index };
+        return circular_index{ (a.m_index + b.m_index) % N };
     }
 
-    auto next() const noexcept -> circular_index { return circular_index{ (0 + m_index + 1) % N }; }
-    auto prev() const noexcept -> circular_index { return circular_index{ (N + m_index - 1) % N }; }
+    constexpr auto next() const noexcept -> circular_index
+    {
+        return circular_index{ (0 + m_index + 1) % N };
+    }
 
-    auto to_size_type() const noexcept -> std::size_t { return m_index; }
+    constexpr auto prev() const noexcept -> circular_index
+    {
+        return circular_index{ (N + m_index - 1) % N };
+    }
+
+    constexpr auto to_size_t() const noexcept -> std::size_t { return m_index; }
 
 private:
     std::size_t m_index{};
@@ -87,7 +94,7 @@ public:
 
     auto size() const noexcept -> std::size_t
     {
-        return (N + m_end.to_size_type() - m_beg.to_size_type()) % N;
+        return (N + m_end.to_size_t() - m_beg.to_size_type()) % N;
     }
 
     auto is_empty() const noexcept -> bool { return m_beg == m_end; }
